c09357385ac4c78ed5e526e3ee192f27025da56d
Mohnish Devadiga <mohnish226@yahoo.co.in>
2018-01-30 21:05:11 +0530

    wikipedia content

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import file_reader
 import termFreq
+import wiki_read
+
 
 # DOCX Correct File
 print(file_reader.read_file('/Users/Mohnish_Devadiga/Desktop/Data/resume.docx'))
@@ -9,3 +11,10 @@ print(file_reader.read_docx('/Users/Mohnish_Devadiga/Desktop/Data/Error-Project-
 print(file_reader.read_file('/Users/Mohnish_Devadiga/Desktop/Data/resume.pdf'))
 
 #termFreq.run_tfidf()
+
+wiki_read.topic("Automatic_summarization")
+url = "https://en.wikipedia.org/wiki/Battle_of_Plassey"
+if "wikipedia.org/wiki" in url:
+    wiki_read.link(url)
+else:
+    print("Topic name")
--- wiki_read.py
+++ wiki_read.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
 import wikipedia
+import os
 
-#print(wikipedia.summary("Automatic Summarization"))
-data = wikipedia.page("Barack")
-print(data.title)
-print(data.content)
-print(data.url)
+
+
+def topic(topic_name):
+    try:
+        data = wikipedia.page(topic_name)
+        '''
+        print(data.title)
+        print(data.content)
+        print(data.url)
+        '''
+        #output_file = filepath+"/output.txt"
+        #f = open(output_file,'w')
+        if not data.content:
+            print("Error! No Content found")
+        else:
+            print(data.content)
+    except:
+        print("Page Error!")
+    return()
+    
+def link(url):
+    url_topic = url.split("/")
+    #print(url_topic[-1])
+    topic_name = ""
+    topic_name += url_topic[-1]
+    #topic_name = [word.replace("/"," ") for word in url_topic[-1]]
+    #print(topic_name.replace("_"," "))
+    topic(topic_name.replace("_"," "))
+    return
